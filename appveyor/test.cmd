bbeddb4869acae3c5bb409839ef6186f1e31f60a
Derick Rethans <github@derickrethans.nl>
2017-12-01 10:37:13 +0000

    And now correctly

--- appveyor/test.cmd
+++ appveyor/test.cmd
@@ -1,12 +1,19 @@
 @echo off
 
-set SDK_RUNNER=%PHP_BUILD_CACHE_SDK_DIR%\phpsdk-%PHP_BUILD_CRT%-!ARCH!.bat
-if not exist "%SDK_RUNNER%" (
-    echo "%SDK_RUNNER%" doesn't exist
-    exit /b 3
-)
+setlocal enableextensions enabledelayedexpansion
+	for %%a in (%ARCHITECTURES%) do (
+		set ARCH=%%a
 
-cmd /c %SDK_RUNNER% -t %APPVEYOR_BUILD_FOLDER%\appveyor\test_task.bat
-if %errorlevel% neq 0 exit /b 3
+		set SDK_RUNNER=%PHP_BUILD_CACHE_SDK_DIR%\phpsdk-%PHP_BUILD_CRT%-!ARCH!.bat
+		if not exist "%SDK_RUNNER%" (
+			echo "%SDK_RUNNER%" doesn't exist
+			exit /b 3
+		)
+
+		cmd /c %SDK_RUNNER% -t %APPVEYOR_BUILD_FOLDER%\appveyor\test_task.bat
+		if %errorlevel% neq 0 exit /b 3
+
+	)
+endlocal
 
 exit /b 0
